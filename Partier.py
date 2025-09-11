b12b583d92c29de7d7a7ee60e9c8b71d86696812
Mari <mahoa077@osloskolen.no>
2025-09-11 11:25:04 +0200

    før lunsj torsdag

--- Partier.py
+++ Partier.py
@@ -1,10 +1,15 @@
 
 
-partier = ["arbeiderpartiet", "miljøpartet de grønne", "høyre", "fremskrittpartiet", "rødt", "senterpartiet", "sosialistisk venstre", "kristelig folkeparti", "venstre"]
-stemmer = ["0", "0", "0", "0", "0", "0", "0", "0", "0"]
+partier = ["ap", "mdg", "h", "frp", "r", "sp", "sv", "krf", "v"]
+stemmer = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+def prosent_regning(stemmer):
+    total = sum(stemmer)
+    prosent = stemmer[i]
+    svar = (prosent / total) * 100
+    return svar
 
 should_continue = True
-
 
 while should_continue:
     print (f"Du kan stemme disse partiene {partier}")
@@ -19,3 +24,15 @@ while should_continue:
     elif input == "stemme":
         print("Ny person kan stemme")
         input("Stem: ")
+    for i in range(len(partier)):
+        if stemme == partier[i]:
+           stemmer[i] += 1
+           print (stemmer[i])
+
+
+for i in range(len(partier)):
+    print(f"{partier[i]}: {stemmer[i]}")
+
+print("Prosenten")
+for i in range(len(stemmer)):
+    print(prosent_regning(stemmer)).round(1)
